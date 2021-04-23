b3684c38835747fa22140280b2a657c341d8d74c
SinusP-CW90 <73338947+SinusP-CW90@users.noreply.github.com>
2021-04-23 10:30:44 +0200

    - test coverage for the case class BattlefieldSpec

--- src/test/scala/de/htwg/se/battleship/model/BattlefieldSpec.scala
+++ src/test/scala/de/htwg/se/battleship/model/BattlefieldSpec.scala
@@ -5,32 +5,40 @@ import org.scalatest.matchers.should.Matchers
 
 class BattlefieldSpec extends AnyWordSpec with Matchers  {
   //TODO Test anpassen!
-/*
   "The Battlefield" when {
+    //case class test for 100% Coverage
+    "initialized" should {
+      "have a  parameter" in {
+        val caseClassPlayground = new Battlefield(Matrix(Vector(Vector(Cell(1)))))
+        Battlefield.unapply(caseClassPlayground).get should be(Matrix(Vector(Vector(Cell(1)))))
+      }
+    }
+  }
+  /*
     "not set to any value " should {
-      val emptyPlayground = Battlefield(0)
+      val emptyPBattlefield = Battlefield(0)
       "have value 0" in {
-        emptyPlayground.width should be(0)
-        emptyPlayground.hight should be(0)
+        emptyBattlefield.width should be(0)
+        emptyBattlefield.hight should be(0)
       }
       "not be set" in {
-        emptyPlayground.widthAndHeightIsSet should be(false)
+        emptyBattlefield.widthAndHeightIsSet should be(false)
       }
     }
     "set to a specific value" should {
-      val nonEmptyPlayground = Battlefield(1,1)
+      val nonEmptyBattlefield = Battlefield(1,1)
       "return that value" in {
-        nonEmptyPlayground.width should be(1)
-        nonEmptyPlayground.hight should be(1)
+        nonEmptyBattlefield.width should be(1)
+        nonEmptyBattlefield.hight should be(1)
       }
       "be set" in {
-        nonEmptyPlayground.widthAndHeightIsSet should be(true)
+        nonEmptyBattlefield.widthAndHeightIsSet should be(true)
       }
     }
-    "PlaygroundString function" should {
-      val nonEmptyPlaygroundString = Battlefield(0,0).playgroundString()
+    "BattlefieldString function" should {
+      val nonEmptyBattlefieldString = Battlefield(0,0).playgroundString()
       "retrun a non empty String" in {
-        nonEmptyPlaygroundString should not be empty
+        nonEmptyBattlefieldString should not be empty
       }
     }
   }
