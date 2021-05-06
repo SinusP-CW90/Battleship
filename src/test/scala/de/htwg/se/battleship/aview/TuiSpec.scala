272fd4046c75fe776bef183e216ce7fd8c51d9ce
SinusP-CW90 <73338947+SinusP-CW90@users.noreply.github.com>
2021-05-07 01:01:33 +0200

    - add new tests for Tui in TuiSpec (all, just start() ist left

--- src/test/scala/de/htwg/se/battleship/aview/TuiSpec.scala
+++ src/test/scala/de/htwg/se/battleship/aview/TuiSpec.scala
@@ -11,6 +11,10 @@ class TuiSpec extends AnyWordSpec with Matchers {
     val controller = new Controller(new Battlefield(6),new Battlefield(6))
     val tui = new Tui(controller)
 
+    "do nothing on input 'q'" in {
+      tui.processInputLine("q")
+    }
+
     "create and empty Battlefield on input 'm'" in {
       tui.processInputLine("m")
       controller.pgP1L should be(new Battlefield(6))
@@ -25,6 +29,11 @@ class TuiSpec extends AnyWordSpec with Matchers {
       tui.processInputLine("l")
       controller.pgP1L should be(new Battlefield(9))
       controller.pgP2R should be(new Battlefield(9))
+    }
+    "do nothing on bad input like'99999'" in {
+      val old = controller.playgroundToString
+      tui.processInputLine("X")
+      controller.playgroundToString should be(old)
     }
     /*
     "quit wenn you type 'q'" in {
