26222a7ea4a26675365c5e6c008d60791136decd
SinusP-CW90 <73338947+SinusP-CW90@users.noreply.github.com>
2021-10-18 21:09:29 +0200

    update librarys

--- build.sbt
+++ build.sbt
@@ -1,21 +1,21 @@
 
 name := "Battleship"
-version := "0.5"
+version := "0.9"
 
-scalaVersion := "2.13.3"
+scalaVersion := "2.13.6"
 
-libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.2"
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.2" % "test"
-libraryDependencies += "org.scalatest" %% "scalatest-wordspec" % "3.2.2" % "test"
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.9"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.9" % "test"
+libraryDependencies += "org.scalatest" %% "scalatest-wordspec" % "3.2.9" % "test"
 
 libraryDependencies += "org.scala-lang.modules" %% "scala-swing" % "3.0.0"
 
 libraryDependencies += "com.google.inject" % "guice" % "5.0.1"
 libraryDependencies += "com.google.inject.extensions" % "guice-assistedinject" % "5.0.1"
-libraryDependencies += "net.codingwell" %% "scala-guice" % "5.0.1"
+libraryDependencies += "net.codingwell" %% "scala-guice" % "5.0.2"
 
 // https://mvnrepository.com/artifact/org.scala-lang.modules/scala-xml
-libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "2.0.0"
+libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "2.0.1"
 
 // https://mvnrepository.com/artifact/com.typesafe.play/play-json
 libraryDependencies += "com.typesafe.play" %% "play-json" % "2.10.0-RC2"
--- src/test/scala/de/htwg/se/battleship/util/CommandSpec.scala
+++ src/test/scala/de/htwg/se/battleship/util/CommandSpec.scala
@@ -9,9 +9,9 @@ class incrCommand extends Command {
   var state:Int =0
   override def doStep(): Try[_] = Try (state+=1)
 
-  override def undoStep: Try[_] = Try (state-=1)
+  override def undoStep(): Try[_] = Try (state-=1)
 
-  override def redoStep: Try[_] = Try (state+=1)
+  override def redoStep(): Try[_] = Try (state+=1)
 }
 
 class CommandSpec extends AnyWordSpec with Matchers {
@@ -20,28 +20,28 @@ class CommandSpec extends AnyWordSpec with Matchers {
     "have a do step" in {
       val command = new incrCommand
       command.state should be(0)
-      command.doStep
+      command.doStep()
       command.state should be(1)
-      command.doStep
+      command.doStep()
       command.state should be(2)
 
     }
     "have an undo step" in {
       val command = new incrCommand
       command.state should be(0)
-      command.doStep
+      command.doStep()
       command.state should be(1)
-      command.undoStep
+      command.undoStep()
       command.state should be(0)
     }
     "have a redo step" in {
       val command = new incrCommand
       command.state should be(0)
-      command.doStep
+      command.doStep()
       command.state should be(1)
-      command.undoStep
+      command.undoStep()
       command.state should be(0)
-      command.redoStep
+      command.redoStep()
       command.state should be(1)
     }
   }
